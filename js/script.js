34ecc1a3f563618c322eba0708d776dbe7659789
IanPedr <iamnascimento2@gmail.com>
2023-10-27 12:15:00 -0300

    projeto_crud

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const tbody = document.querySelector("tbody");
+const descItem = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+const type = document.querySelector("#type");
+const btnNew = document.querySelector("#btnNew");
+
+const incomes = document.querySelector(".incomes");
+const expenses = document.querySelector(".expenses");
+const total = document.querySelector(".total");
+
+console.log("Projeto Crud")
